9441363c8573ff2694e39612ff3f71c931dc2684
jackal998 <jackal998@gmail.com>
2016-10-27 20:49:18 +0800

    Day10_ELRUMI_RWD

---
+++ Day9_20161026/prec.rb
@@ -0,0 +1,21 @@
+class Template 
+	def initialize(name,a)
+		@name = name
+		@na =a
+	end
+end
+
+module N
+	def foo(x)
+	puts x
+end
+end
+
+class Temp
+	include N
+end
+
+myname = Template.new("EJ","a")
+
+my = Temp.new()
+my.foo("HA")
